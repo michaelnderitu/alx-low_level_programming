f54fda275d7f834837f31838c46e79549f14f4e3
michaelnderitu <michaelnderitu2010@outlook.com>
2023-03-20 06:11:01 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-printf("Size of char: %d byte(s)\n", sizeof(char));
-printf("Size of int: %d byte(s)\n", sizeof(int));
-printf("Size of float: %lu bytes(s)\n", sizeof(float));
-printf("Size of double: %lu byte(s)\n", sizeof(double));
-printf("Size of long: %lu byte(s)\n", sizeof(long));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of a int: %d byte(s)\n", sizeof(int));
+printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+printf("Size of a double: %lu byte(s)\n", sizeof(double));
+printf("Size of a long: %lu byte(s)\n", sizeof(long));
 return (0);
 }
