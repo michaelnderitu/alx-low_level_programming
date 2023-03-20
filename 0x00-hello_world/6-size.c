4e8766b68ecbc0ffb0f87c861baf634762c66822
michaelnderitu <michaelnderitu2010@outlook.com>
2023-03-20 04:39:46 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * printf - prints the size of the computer
+ * return - Always 0
+ */
+
+int main()
+{
+printf("Size of char:%lu\n",sizeof(char));
+printf("Size of int:%lu\n",sizeof(int));
+printf("Size of float:%lu\n",sizeof(float));
+printf("Size of double:%lu\n",sizeof(double));
+printf("Size of long:%lu\n",sizeof(long));
+
+return 0;
+}
