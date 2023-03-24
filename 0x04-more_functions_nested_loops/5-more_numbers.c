4d595a888c68be52104283cc2e5d26a4a1042ef5
michaelnderitu <michaelnderitu2010@outlook.com>
2023-03-24 10:26:36 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,12 +6,15 @@
  */
 void more_numbers(void)
 {
-int c;
-for (c = 0; c <= 14; c++)
+int i, j;
+for (i = 1; i <= 10; i++)
 {
-while (c < 11)
+for (j = 0; j < 15; j++)
 {
-_putchar(c);
+if (j >= 10)
+_putchar('1');
+_putchar(j % 10 + '0');
 }
+_putchar('\n');
 }
 }
