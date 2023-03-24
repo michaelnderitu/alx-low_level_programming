4f496b69938d8c9e108b8efd4765733d54ec7d1e
michaelnderitu <michaelnderitu2010@outlook.com>
2023-03-24 03:56:53 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *more_numbers - function that repeats numbers 0 to 14 ten times
+ *for - loop through our function
+ *while - loop through our for loop
+ */
+void more_numbers(void)
+{
+int c;
+for (c = 0; c <= 14; c++)
+{
+while (c < 11)
+{
+_putchar(c);
+}
+}
+}
