76fe72b2de015cd8e9c5fd3d04057993e8b76477
michaelnderitu <michaelnderitu2010@outlook.com>
2023-03-23 18:35:56 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_numbers - function that prints numbers
+ *for: a loop that is in our function
+ */
+void print_numbers(void)
+{
+int c;
+for (c = 0; c < 10; c++)
+{
+_putchar(c);
+}
+}
