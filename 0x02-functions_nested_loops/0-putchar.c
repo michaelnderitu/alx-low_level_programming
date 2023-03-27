4e8537887dc54c19cd2dbe6a3b386f452725b230
michaelnderitu <michaelnderitu2010@outlook.com>
2023-03-27 04:05:13 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,14 @@
  */
 int main(void)
 {
-char c;
-c = "_putchar";
-_putchar(c);
+_putchar('_');
+_putchar('P');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 
 return (0);
