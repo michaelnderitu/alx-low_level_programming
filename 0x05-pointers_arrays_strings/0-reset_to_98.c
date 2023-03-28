47f95b27cf3f0b40179788964749a3fd67469630
michaelnderitu <michaelnderitu2010@outlook.com>
2023-03-28 02:16:01 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *reset_to_98 - dereferencing integer to 98
+ *@n: pointer variable pointing at an integer
+ *Return: void
+ */
+void reset_to_98(int *n)
+{
+int p = 402;
+*n = p;
+
+*n = 98;
+}
