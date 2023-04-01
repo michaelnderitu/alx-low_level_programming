1f652a5e8ba586263385f975b694cc8f02f61013
michaelnderitu <michaelnderitu2010@outlook.com>
2023-03-31 18:54:25 -0700

    reverse integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *reverse_array - reverse integers
+ *@a: integer 1
+ *@n: integer 2
+ */
+void reverse_array(int *a, int n)
+{
+int i;
+int j;
+
+for (i = n - 1; i > n / 2; i--)
+{
+j = a[n - 1 - i];
+a[n - 1 - i] = a[i];
+a[i] = j;
+}
+}
